f909aa06e98061d9341c0bad6a2d1ac3e7337b23
Jonathan C. Cantwell <jon.c.cantwell@gmail.com>
2018-11-10 22:58:07 -0500

    completely redid day 10's core logic to not suck

--- Advent/Year2017/Day10.pm
+++ Advent/Year2017/Day10.pm
@@ -50,12 +50,34 @@ sub b  {
     push @bytes, (17, 31, 73, 47, 23);
 
     my @hash = (0..255);
-    my $hashref = \@hash;
     my $skip = 0;
-    foreach my $i (0..63) {
-        ($skip, $hashref) = round($skip, $hashref, @bytes);
+    my $position = 0;
+
+    # fuck all of this and start over
+
+    for (my $round = 0; $round < 64; $round++) {
+        #say "round $round, hash is @hash";
+        foreach my $byte (@bytes) {
+            #say "processing byte $byte";
+            my $term = ($position + $byte - 1) % @hash;
+            my $curr = $position;
+
+            #say "swapping between $curr and $term";
+
+            for(my $i = 0; $i < ($byte / 2); $i++) {
+                # swap
+                my $temp = $hash[$curr];
+                $hash[$curr] = $hash[$term];
+                $hash[$term] = $temp;
+                $term -= 1;
+                $curr += 1;
+                $term = $term < 0 ? $#hash : $term;
+                $curr = $curr > $#hash ? 0 : $curr;
+            }
+            $position = ($position + $byte + $skip++) % (scalar @hash);
+            #say "hash now looks like @hash and current position is $position";
+        }
     }
-    @hash = @$hashref;
     
     my @dense;
     foreach my $block (0..15) {
@@ -64,36 +86,6 @@ sub b  {
         push @dense, $hex;
     }
     say @dense;
-}
-
-sub round {
-    my $skip = shift;
-    my $listref = shift;
-    my @lengths = @_;
-    my $moved_forward = 0;
-    my $diff = 0;
-    my $len = 256;
-    my @list = @$listref;
-    foreach my $length (@lengths) {
-        push @list, @list;
-        splice @list, 0, $len, reverse splice @list, 0, $length;
-        push @list, @list;
-        push @list, @list;
-        push @list, @list;
-        push @list, @list;
-        $moved_forward += ($length + $skip);
-        @list = splice @list, ($length + $skip), $len;
-        $skip += 1;
-    }
-    
-    $diff = $len - ($moved_forward % $len); 
-    push @list, @list;
-    push @list, @list;
-    push @list, @list;
-    push @list, @list;
-    @list = splice @list, $diff, $len;
-
-    return ($skip, \@list);
 }
 
 1;
---
+++ Advent/Year2017/Day11.pm
@@ -0,0 +1,20 @@
+package Advent::Year2017::Day11;
+
+use 5.28.0;
+use warnings;
+use strict;
+use Exporter qw(import);
+
+our $VERSION     = 1.00;
+our @ISA         = qw(Exporter);
+our @EXPORT_OK   = qw(a b);
+our %EXPORT_TAGS = ( DEFAULT => [qw(a b)] );
+
+sub a  { 
+    say "side a" 
+}
+sub b  { 
+    say "side b" 
+}
+
+1;
